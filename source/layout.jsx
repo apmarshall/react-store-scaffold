081156f413c4cd6a9812c9be7d1e47f9ad65e119
Alex Floyd Marshall <alex@soren.tech>
2019-08-23 15:13:17 -0400

    Fix: wrap render in a return element

--- source/layout.jsx
+++ source/layout.jsx
@@ -1,14 +1,21 @@
 "use strict";
 
 import React from "react";
+import Menu from "./components/menu.jsx";
+import Footer from "./components/footer.jsx";
 
 class Layout extends React.Component {
 	render() {
-		<div>
-			{ React.cloneElement(
-				this.props.children,
-				this.state
-			)}
-		</div>
+		return(
+			<div>
+				<Menu />
+				{ React.cloneElement(
+					this.props.children,
+					this.state
+				)}
+				<Footer />
+			
+			</div>
+		);
 	}
 }
