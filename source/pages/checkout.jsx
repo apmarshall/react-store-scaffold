6b2d90142204a84e995e61e13f1a4ac9a60a8b11
Alex Floyd Marshall <alex@soren.tech>
2019-08-28 06:29:44 -0400

    Adding receipt, checkout, and item "pages" (duplicates of product)

---
+++ source/pages/checkout.jsx
@@ -0,0 +1,13 @@
+"use strict";
+
+import React from "react";
+
+class Checkout extends React.Class {
+    render() {
+        return (
+            <div />
+        );
+    }
+}
+
+module.exports = Checkout;
---
+++ source/pages/item.jsx
@@ -0,0 +1,13 @@
+"use strict";
+
+import React from "react";
+
+class Item extends React.Class {
+    render() {
+        return (
+            <div />
+        );
+    }
+}
+
+module.exports = Item;
