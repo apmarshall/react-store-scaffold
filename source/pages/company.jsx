ceadddfac0253411d5a714d958ec244f1d968c16
Alex Floyd Marshall <alex@soren.tech>
2019-08-28 06:30:27 -0400

    Adding company info page
    To Do: Needs layout components/styling

---
+++ source/pages/company.jsx
@@ -0,0 +1,16 @@
+"use strict";
+import React from "react";
+
+class Company extends React.Class {
+    render() {
+        return (
+            <h1>The company</h1>
+            
+            <p>Contact information</p>
+            <p>Phone number</p>
+            <p>History of the our company</p>
+        );
+    }
+}
+
+module.exports = Company;
